718ddc71639a0a53602e5fae5b762f1d95ba2176
RohitKumarPatel2004 <rohitpatel843332@gmail.com>
2024-02-29 18:20:50 +0530

    formating

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ setInterval(() => {
     let min = date.getMinutes();
     let sec = date.getSeconds();
 
-    let session="AM";
+    let session = "AM";
 
-    if(hour>12){
-        hour=hour-12
-        session="PM";
+    if (hour > 12) {
+        hour = hour - 12
+        session = "PM";
     }
 
     let h = hour <= 9 ? "0" + hour : hour
     let m = min <= 9 ? "0" + min : min
     let s = sec <= 9 ? "0" + sec : sec
 
-   
+
 
     let time = document.querySelector(".clock");
     time.innerText = `${h}  : ${m}  : ${s}   ${session}`;
-},1000)
+}, 1000)
